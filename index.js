6043ba2a2c1dee149ceb558f480a639604cc8761
Oliver <oliveroneill04@gmail.com>
2017-10-17 22:01:43 +1100

    Add initial code for running script and creating logs

--- index.js
+++ index.js
@@ -1,2 +1,36 @@
 #!/usr/bin/env node
-console.log("TODO")
+console.log("Starting bert. Type 'exit' when you're done.")
+// TODO: run file watcher
+
+// TODO: add command line option to specify logs location
+let logDir = resolveHome('~/.bert/');
+
+// create the log directory if it doesn't already exist
+createDirectory(logDir);
+
+// start `script`
+var spawn = require('child_process').spawn;
+// we won't intercept the output to avoid causing user disruption
+spawn('script', ['-F', logDir + generateNewLogName()], { stdio: 'inherit' });
+
+// generate new log to avoid name conflicts
+function generateNewLogName() {
+    // TODO: check directory and add increment for each new session
+    return 'log';
+}
+
+function createDirectory(dir) {
+    const fs = require('fs');
+    if (!fs.existsSync(dir)){
+        fs.mkdirSync(dir);
+    }
+}
+
+// resolve ~ as the user's home directory
+function resolveHome(filepath) {
+    const path = require('path');
+    if (filepath[0] === '~') {
+        return path.join(process.env.HOME, filepath.slice(1));
+    }
+    return filepath;
+}
