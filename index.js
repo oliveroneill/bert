5d63eada183993546ede456ec4b23f18ca28ddb5
Oliver <oliveroneill04@gmail.com>
2017-10-18 20:29:20 +1100

    Cleanup

--- index.js
+++ index.js
@@ -5,20 +5,6 @@ const fs = require('fs');
 const path = require('path');
 const spawn = require('child_process').spawn;
 
-if (require.main === module) {
-  console.log("Starting bert. Type 'exit' when you're done.")
-  // TODO: run file watcher
-
-  // TODO: add command line option to specify logs location
-  let logDir = resolveHome('~/.bert/');
-
-  // create the log directory if it doesn't already exist
-  createDirectory(logDir);
-
-  // start `script`
-  // we won't intercept the output to avoid causing user disruption
-  spawn('script', ['-q', '-F', logDir + generateNewLogName()], { stdio: 'inherit' });
-}
 // generate new log to avoid name conflicts
 function generateNewLogName() {
   // TODO: check directory and add increment for each new session
@@ -37,4 +23,23 @@ function resolveHome(filepath) {
     return path.join(process.env.HOME, filepath.slice(1));
   }
   return filepath;
+}
+
+function main() {
+  console.log("Starting bert. Type 'exit' when you're done.");
+  // TODO: run file watcher
+
+  // TODO: add command line option to specify logs location
+  let logDir = resolveHome('~/.bert/');
+
+  // create the log directory if it doesn't already exist
+  createDirectory(logDir);
+
+  // start `script`
+  // we won't intercept the output to avoid causing user disruption
+  spawn('script', ['-q', '-F', logDir + generateNewLogName()], { stdio: 'inherit' });
+}
+
+if (require.main === module) {
+  main();
 }
