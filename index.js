179aa53ae8a4020cf4559aa7e75502136b98d81d
Oliver <oliveroneill04@gmail.com>
2017-10-18 20:25:24 +1100

    Change indent spacing

--- index.js
+++ index.js
@@ -5,35 +5,36 @@ const fs = require('fs');
 const path = require('path');
 const spawn = require('child_process').spawn;
 
-console.log("Starting bert. Type 'exit' when you're done.")
-// TODO: run file watcher
+if (require.main === module) {
+  console.log("Starting bert. Type 'exit' when you're done.")
+  // TODO: run file watcher
 
-// TODO: add command line option to specify logs location
-let logDir = resolveHome('~/.bert/');
+  // TODO: add command line option to specify logs location
+  let logDir = resolveHome('~/.bert/');
 
-// create the log directory if it doesn't already exist
-createDirectory(logDir);
+  // create the log directory if it doesn't already exist
+  createDirectory(logDir);
 
-// start `script`
-// we won't intercept the output to avoid causing user disruption
-spawn('script', ['-q', '-F', logDir + generateNewLogName()], { stdio: 'inherit' });
-
+  // start `script`
+  // we won't intercept the output to avoid causing user disruption
+  spawn('script', ['-q', '-F', logDir + generateNewLogName()], { stdio: 'inherit' });
+}
 // generate new log to avoid name conflicts
 function generateNewLogName() {
-    // TODO: check directory and add increment for each new session
-    return 'log';
+  // TODO: check directory and add increment for each new session
+  return 'log';
 }
 
 function createDirectory(dir) {
-    if (!fs.existsSync(dir)){
-        fs.mkdirSync(dir);
-    }
+  if (!fs.existsSync(dir)){
+    fs.mkdirSync(dir);
+  }
 }
 
 // resolve ~ as the user's home directory
 function resolveHome(filepath) {
-    if (filepath[0] === '~') {
-        return path.join(process.env.HOME, filepath.slice(1));
-    }
-    return filepath;
+  if (filepath[0] === '~') {
+    return path.join(process.env.HOME, filepath.slice(1));
+  }
+  return filepath;
 }
